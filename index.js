bebb42123fa65ced9c2a48864898a2232f20c59f
unknown <koknemr@gmail.com>
2021-01-10 19:13:19 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.set("port", (process.env.port || 8000));
+
+app.use(bodyparser.urlencoded( {extended: false} ));
+
+app.use(bodyparser.json());
+
+app.get("/", function(req, res){
+    res.send("مرحبا بك في منظومة الماسنجر");
+});
+
+app.listen(app.get("port"), function(){
+    console.log("server is running and listening on port " + app.get("port"));
+});
