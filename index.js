7b54b01f87135e242074bd0ac9e2339cce0f79cd
khalifa <koknemr@gmail.com>
2021-01-10 20:35:00 +0200

    adding webhook verify

--- index.js
+++ index.js
@@ -6,14 +6,28 @@ const app = express();
 
 app.set("port", (process.env.PORT || 8000));
 
-app.use(bodyparser.urlencoded( {extended: false} ));
+app.use(bodyparser.urlencoded({ extended: false }));
 
 app.use(bodyparser.json());
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.send("مرحبا بك في منظومة الماسنجر");
 });
 
-app.listen(app.get("port"), function(){
+app.get("/webhook", function (req, res) {
+    const PAGE_VERIFY_TOKEN = "khalifa0913964002";
+
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    if (token === PAGE_VERIFY_TOKEN) {
+        res.status(200).send(challenge);
+    }
+    else{
+        res.status(403);
+    }
+});
+
+app.listen(app.get("port"), function () {
     console.log("server is running and listening on port " + app.get("port"));
 });
